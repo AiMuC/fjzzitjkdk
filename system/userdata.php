b98c8dffd383f96fd23f27b52819c71fca96a52a
AiMuC <31570354+AiMuC@users.noreply.github.com>
2021-02-13 17:30:53 +0800

    Delete system directory

--- system/userdata.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$insertdata = array(
-	'1'=>array('dq'=>["福建省","漳州市","芗城区"],'sign'=>'小程序签名 需抓包获取','userCode'=>'学生学号','userName'=>'学生姓名','email'=>'QQ邮箱地址'),
-	);//用户信息内容
-
-//print_r($insertdata);//打印用户信息内容
-
-?>
